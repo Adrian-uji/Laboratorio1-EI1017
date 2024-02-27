2bbf55ddf3b1f12059a2afc27fc24e5df9d7ea38
Adrián Soriano Morató <infosm2004@gmail.com>
2024-02-27 11:11:15 +0100

    Terminados los reads

--- src/main/java/CSV.java
+++ src/main/java/CSV.java
@@ -1,12 +1,14 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 
 public class CSV {
+    //Contador de columnas
+    int cont = 0;
     //Métodos
-    public Table readTable(String document) throws IOException { //En lugar de "static void" va Table
+
+    //Métodos read
+    public Table readTable(String document) throws IOException {
         File archivo = new File(document);
         if (archivo.exists()){
             FileReader fr = new FileReader(archivo);
@@ -31,6 +33,34 @@ public class CSV {
             throw new FileNotFoundException();
         }
     }
+    public TableWithLabels readTableWithLabels (String document) throws IOException {
+        Map<String, Integer> labelsToolIndex = new HashMap<>();
+        File archivo = new File(document);
+        if (archivo.exists()){
+            FileReader fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            int i = 0;
+            List<String> headers = new LinkedList<String>();
+            List<RowWithLabel> rows = new ArrayList();
+            while((linea = br.readLine()) != null){
+                if (i<1){
+                    headers = readHeaders(linea);
+                }
+                else{
+                    rows.add(readRowsWithLable(linea, labelsToolIndex));
+                }
+                i++;
+            }
+
+            TableWithLabels table = new TableWithLabels(headers,rows,labelsToolIndex);
+            return table;
+        }
+        else{
+            throw new FileNotFoundException();
+        }
+    }
+    //Métodos auxiliares
     public List<String> readHeaders(String linea) {
         List<String> header = new ArrayList<>();
         String words = "";
@@ -39,7 +69,11 @@ public class CSV {
             if (caracter != ',') {
                 words = words + caracter;
             } else {
-                header.add(words);
+                if (!words.equals("class"))
+                {
+                    header.add(words);
+                    cont++;
+                }
                 words = "";
             }
         }
@@ -61,7 +95,31 @@ public class CSV {
         rows = (Row) rowList;
         return rows;
     }
-    public TableWithLabels readTableWithLabels (String document) throws IOException {
-        //A completar
+    public RowWithLabel readRowsWithLable(String linea, Map<String, Integer> labelsMap){
+        int numberClass;
+        List<Double> rowList = new LinkedList<>();
+        String numberString = "";
+        for (int i = 0; i < cont; i++) {
+            char caracter = linea.charAt(i);
+            if (caracter != ',') {
+                numberString = numberString + caracter;
+            } else {
+                rowList.add(Double.parseDouble(numberString));
+                numberString = "";
+            }
+        }
+        String identifier = "";
+        for (int i = cont; i<linea.length();i++){
+            identifier += linea.charAt(i);
+        }
+        if (labelsMap.containsKey(identifier)){
+            numberClass = labelsMap.get(identifier);
+        }
+        else{
+            numberClass = labelsMap.size();
+            labelsMap.put(identifier,numberClass);
+        }
+        RowWithLabel row = new RowWithLabel(rowList,numberClass);
+        return row;
     }
 }
